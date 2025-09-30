f149bcfbae11e30159ec8d4d90751d4ec4204162
abdullah-aladham <abdullah.aladham@gmail.com>
2025-10-01 01:09:46 +0300

    updated theme colors and fonts

--- ar/js/index.js
+++ ar/js/index.js
@@ -7,12 +7,16 @@ checkbox.addEventListener("change",()=>{
     if(checkbox.checked){
         html.setAttribute('data-bs-theme',"dark");
         navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-dark");
-    body.setAttribute("color","#e0e1dd");
-        
+    // body.setAttribute("color","#e0e1dd");
+    // body.setAttribute("background-color","#2b3035")
+    body.style.backgroundColor="#2b3035";
+    body.style.color="#5D9D61";
     }else {
         html.setAttribute("data-bs-theme","light");
         navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-light");
         body.setAttribute("color","dark");
+            body.style.backgroundColor="#edece7";
+            body.style.color="#2E6F40";
 
     }
 })
--- js/main.js
+++ js/main.js
@@ -2,16 +2,23 @@ const html= document.getElementById("htmlPage");
 const body=document.getElementById("body");
 const navbar=document.getElementById("navbar")
 const checkbox = document.getElementById("checkbox");
+
 checkbox.addEventListener("change",()=>{
     if(checkbox.checked){
-        html.setAttribute('data-bs-theme',"dark")
-        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-dark")
-    body.setAttribute("color","#e0e1dd")
-        
+        html.setAttribute('data-bs-theme',"dark");
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-dark");
+    // body.setAttribute("color","#e0e1dd");
+    // body.setAttribute("background-color","#2b3035")
+    body.style.backgroundColor="#2b3035";
+    body.style.color="#5D9D61";
+    // navbar.style.color="#5D9D61";
+    // navbar.setAttribute("color","#5D9D61");
     }else {
         html.setAttribute("data-bs-theme","light");
-        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-light")
-        body.setAttribute("color","dark")
+        navbar.setAttribute("class","navbar navbar-expand-lg fixed-top navbar-light bg-light");
+        body.setAttribute("color","dark");
+            body.style.backgroundColor="#edece7";
+            body.style.color="#2E6F40";
 
     }
 })
